f7fd50ff561b1decdfb6e30dca19f4a081116594
reedjh1 <150959383+reedjh1@users.noreply.github.com>
2024-10-18 16:53:22 -0400

    final

--- Program.cs
+++ Program.cs
@@ -7,35 +7,61 @@ namespace test1
         static void Main(string[] args)
         {
             int tokens = 10;
-            int slotEarnings = 0;
-            bool mainRun = true;
+            int slotsEarnings = 0;
+            int hideNSeekEarnings = 0;
+            bool run = true;
 
             do
             {
                 Console.Clear();
+
+                if (tokens >= 20)
+                {
+                    Console.WriteLine("Congratulations! You've passed the adoption test!\nSee you next time!");
+                    run = false;
+                    break;
+                }
+
                 Console.WriteLine("Welcome to the CatCade! =^.^=\nSelect an option:\n1. Slots\n2. Hide'n'Seek\n3. View Token Balance\n4. Exit");
                 int input = GetIntParse(Console.ReadLine(), 1, 4);
 
                 switch (input)
                 {
                     case 1:
-                        RunSlots(ref slotEarnings, ref tokens);
+                        if(slotsEarnings < 6)
+                        {
+                            RunSlots(ref tokens, ref slotsEarnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You've earned all you can from slots!");
+                            Pause();
+                        }
                         break;
                     case 2:
+                        if(hideNSeekEarnings < 6)
+                        {
+                            HideNSeek(ref tokens, ref hideNSeekEarnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You've earned all you can from hide n' seek!");
+                            Pause();
+                        }
                         break;
                     case 3:
-                        ViewTokenBalance(tokens);
+                        ViewTokenOverview(tokens, slotsEarnings, hideNSeekEarnings);
                         break;
                     case 4:
                         Console.WriteLine("Goodbye!");
-                        mainRun = false;
+                        run = false;
                         break;
                     default:
                         Console.WriteLine("Please enter a valid option.");
                         Pause();
                         break;
                 }
-            } while(mainRun);
+            } while(run);
         }
         
         static int GetIntParse(string input, int min, int max)
@@ -52,7 +78,7 @@ namespace test1
                     Console.Write($"ERROR: {input} is invalid! Try again: ");
                     input = Console.ReadLine();
                 }
-                if ((output < min) || (output > max))
+                else if ((output < min) || (output > max))
                 {
                     success = false;
 
@@ -130,10 +156,7 @@ namespace test1
 
                 symbols[2] = symbolBank[random.Next(0, 6)];
 
-                if (Console.IsOutputRedirected == false)
-                {
-                    Console.Clear();
-                }
+                Console.Clear();
 
                 Console.WriteLine($"        Cat Slots\n+-----------------------+\n| {symbols[0]} | {symbols[1]} | {symbols[2]} |\n+-----------------------+");
                 Thread.Sleep(50);
@@ -150,7 +173,7 @@ namespace test1
             return tokens;
         }
 
-        static void RunSlots(ref int slotEarnings, ref int tokens)
+        static void RunSlots(ref int tokens, ref int slotEarnings)
         {
             bool slotsRun = true;
             const int maxWager = 2;
@@ -158,7 +181,7 @@ namespace test1
             do
             {
                 Console.Clear();
-                Console.WriteLine(">->->->-> Cat Slots <-<-<-<-<");
+                Console.WriteLine("---- Cat Slots ----");
                 
                 if (slotEarnings < 6)
                 {
@@ -191,16 +214,129 @@ namespace test1
                 }
             } while(slotsRun);
         }
-
-        static void HideNSeek()
+        
+        static string VerifyGuess(string[] hidingSpots, string input)
         {
-            string[] hidingSpots = { "balcony", "bathtub", "closet", "couch", "fridge", "plant", "sink", "lLlaasd" };
+            bool success = false;
+
+            do
+            {
+                success = hidingSpots.Contains(input);
+
+                if(!success)
+                {
+                    Console.WriteLine("Enter a valid hiding spot!");
+                    input = Console.ReadLine();
+                }
+            } while(!success);
+            
+            return input;
         }
 
-        static void ViewTokenBalance(int tokens)
+        static int GetIndexOf(string[] hidingSpots, string input)
+        {
+            for (int i = 0; i < hidingSpots.Length; i++)
+            {
+                if (hidingSpots[i] == input) return i;
+            }
+
+            return -1;
+        }
+
+        static void HideNSeek(ref int tokens, ref int totalEarnings)
+        {
+            string[] hidingSpots = { "balcony", "bathtub", "closet", "couch", "fridge", "plant", "sink" };
+            bool run = true;
+
+            do
+            {
+                Random random = new Random();
+                int hidingSpot = random.Next(0, hidingSpots.Length);
+                int tries = 3;
+                int[] proximity = { -1, -1};
+                bool won = false;
+
+                Console.Clear();
+                Console.WriteLine("---- Hide N' Seek! ----");
+                Console.WriteLine($"The cat is hiding in 1 of {hidingSpots.Length} places.");
+
+                for(int i = 0; i < hidingSpots.Length; i++)
+                {
+                    Console.WriteLine($"\t{hidingSpots[i]}");
+                }
+
+                Console.WriteLine("You have 3 tries to guess where the cat is hiding! Good luck!");
+
+                while(tries > 0)
+                {
+                    Console.WriteLine("Enter your guess: ");
+                    string input = Console.ReadLine();
+                    string guess = VerifyGuess(hidingSpots, input);
+
+                    tries--;
+
+                    proximity[0] = proximity[1];
+                    proximity[1] = GetIndexOf(hidingSpots, guess);
+
+                    if(proximity[1] == hidingSpot)
+                    {
+                        won = true;
+                        break;
+                    }
+
+                    if(proximity[0] == -1) continue;
+
+                    if(Math.Abs(proximity[1] - hidingSpot) > Math.Abs(proximity[0] - hidingSpot))
+                    {
+                        Console.WriteLine("You're getting colder!");
+                    }
+                    else if (Math.Abs(proximity[1] - hidingSpot) == Math.Abs(proximity[0] - hidingSpot))
+                    {
+                        Console.WriteLine("Your guesses were equally as close!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You're getting warmer!");
+                    }
+                }
+
+                Console.Clear();
+
+                if(won)
+                {
+                    Console.WriteLine("Congratulations! You guessed it!");
+                    tokens += 3;
+                    totalEarnings += 3;
+                }
+                else if(!won)
+                {
+                    Console.WriteLine($"The hiding spot was {hidingSpots[hidingSpot]}.");
+                    Console.WriteLine("Better luck next time! Your tokens are mine now!");
+                    tokens -= totalEarnings;
+                    totalEarnings = 0;
+                }
+
+                Console.WriteLine($"Total earnings: {totalEarnings}");
+
+                if(totalEarnings >= 6)
+                {
+                    Console.WriteLine("You've earned all you can from hide n' seek!");
+                    run = false;
+                    Pause();
+                }
+                else
+                {
+                    run = Replay();
+                }
+            } while(run);
+        }
+
+        static void ViewTokenOverview(int tokens, int slotsEarnings, int hideNSeekEarnings)
         {
             Console.Clear();
-            Console.WriteLine($"Token balance: {tokens}");
+            Console.WriteLine($"You have {tokens} tokens.");
+            Console.WriteLine($"You've earned {slotsEarnings} tokens from slots.");
+            Console.WriteLine($"You've earned {hideNSeekEarnings} tokens from hide n' seek.");
             Pause();
         }
     }
